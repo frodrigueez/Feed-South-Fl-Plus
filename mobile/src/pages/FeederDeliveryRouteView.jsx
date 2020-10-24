d050dfd0e986dea58920547813318a45583bf464
Earl Cameron <mr.e.cameron@gmail.com>
2020-10-24 19:40:51 -0400

    needs logic to hide components

--- mobile/src/pages/FeederDeliveryRouteView.jsx
+++ mobile/src/pages/FeederDeliveryRouteView.jsx
@@ -17,6 +17,22 @@ const FeederDeliveryRouteView = () => {
         <div className={`${style.menuTop} px-5 pt-5`}>
           <div className="container-fluid">
             <div className="row">
+              <div className="col-12 p-0" style={{color:'#436026', fontWeight:'600'}}>Arriving in 10 minutes</div>
+              <div className={`${style.status} col-12 px-2 py-3 mb-5 mt-3`}>
+                <div className="row px-3">
+                  <div className="col-4 p-0">
+                    <input type="checkbox" name="" id="" checked /> Picked
+                  </div>
+                  <div className="col-4 p-0">
+                    <input type="checkbox" name="" id="" checked /> In transit
+                  </div>
+                  <div className="col-4 p-0">
+                    <input type="checkbox" name="" id="" /> Delivered
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
               <div className="col-2 p-0 text-center">
                 <img src={profile} alt="" className="shadow" />
               </div>
@@ -27,12 +43,12 @@ const FeederDeliveryRouteView = () => {
                 Feeder Full name
               </div>
               <div className="col-2 p-0 text-center">
-                <img src={phone} alt="" height='39px' />
+                <img src={phone} alt="" height="39px" />
               </div>
             </div>
             <div className="row">
               <div className={`${style.card} col-12 mt-5 p-2 border`}>
-                  <img src={card} alt="" className='img-fluid'/>
+                <img src={card} alt="" className="img-fluid" />
               </div>
             </div>
           </div>
