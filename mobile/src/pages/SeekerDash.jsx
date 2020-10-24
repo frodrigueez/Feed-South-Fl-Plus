f22779e8bf795d073e987f6eac7bb9b13fbc2733
Earl Cameron <mr.e.cameron@gmail.com>
2020-10-24 15:30:37 -0400

    page 5 markup complete

--- mobile/src/pages/SeekerDash.jsx
+++ mobile/src/pages/SeekerDash.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./SeekerDash.module.css";
 import seekerSignUp from "../assets/images/seekersignup.png";
+import deliveryicon from "../assets/images/deleiveryiconorange.png";
+import locatioMarker from "../assets/images/locationmarkgreen.png";
 const SeekerDash = () => {
   return (
     <div className={`${style.main}`}>
@@ -8,12 +10,12 @@ const SeekerDash = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-8 mx-auto text-center">
-              <img src={seekerSignUp} alt="" className="" height='350px' />
+              <img src={seekerSignUp} alt="" className="" height="350px" />
             </div>
           </div>
         </div>
       </div>
-      <div className={`${style.welcome} text-center`}>Welcome,</div>
+      <div className={`${style.welcome} text-center`}><b>Welcome,</b></div>
       <div className={`${style.welcomeMessage} text-center`}>
         We are here to help
       </div>
@@ -21,8 +23,24 @@ const SeekerDash = () => {
         <div className="container">
           <div className="row">
             <div className="col-10 mx-auto">
-              <button className={`${style.distBt} btn btn-block p-3`}>
-                Food Distrubution
+              <button className={`${style.distBt} btn btn-block p-2`}>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-1">
+                      {" "}
+                      <img
+                        src={locatioMarker}
+                        alt=""
+                        style={{
+                          filter: "grayscale(100%) invert(100%) brightness(2)",
+                        }}
+                      />{" "}
+                    </div>
+                    <div className="col-auto ml-auto pt-2 text-right pr-4">
+                      Food Distrubution
+                    </div>
+                  </div>
+                </div>
               </button>
             </div>
           </div>
@@ -33,7 +51,23 @@ const SeekerDash = () => {
           <div className="row">
             <div className="col-10 mx-auto">
               <button className={`${style.deleveryBt} btn btn-block p-3`}>
-                Food Delivery
+                <div className="container">
+                  <div className="row">
+                    <div className="col-1 p-0">
+                      {" "}
+                      <img
+                        src={deliveryicon}
+                        alt=""
+                        style={{
+                          filter: "grayscale(100%) invert(100%) brightness(2)",
+                        }}
+                      />{" "}
+                    </div>
+                    <div className="col-auto ml-auto pt-1 text-right pr-4">
+                      Food Delivery
+                    </div>
+                  </div>
+                </div>
               </button>
             </div>
           </div>
