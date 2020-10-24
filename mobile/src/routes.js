6cadf2982849614a2f1a713aa5c714b4b9f1bc3d
Nicolas Maltais <nic.maltais@gmail.com>
2020-10-24 14:30:29 -0400

    Add UserType page

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {HashRouter, Switch, Route} from 'react-router-dom'
-import Login from './pages/Login'
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import UserType from './pages/UserType';
 const Router = () => {
     return ( 
         <HashRouter>
             <Switch>
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/usertype" component={()=><UserType />} />
             </Switch>
         </HashRouter>
      );
