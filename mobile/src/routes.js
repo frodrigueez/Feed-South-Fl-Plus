d881d55995049594c4a8b92dafec4b5fa2085dd4
Earl Cameron <mr.e.cameron@gmail.com>
2020-10-24 14:23:13 -0400

    seeker map init

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {HashRouter, Switch, Route} from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import SeekerMapView from './pages/SeekerMapPage'
 const Router = () => {
     return ( 
         <HashRouter>
             <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
+                <Route exact path="/seeker" component={SeekerMapView} />
             </Switch>
         </HashRouter>
      );
