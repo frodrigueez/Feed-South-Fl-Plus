244fe13d54880e60b74f6a0eaa97ece7457438c8
Earl Cameron <mr.e.cameron@gmail.com>
2020-10-24 22:37:14 -0400

    last changes

--- mobile/src/pages/FeederDistRouteView.jsx
+++ mobile/src/pages/FeederDistRouteView.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import style from "./FeederDistRouteView.module.css";
 import mapBg from "../assets/images/map.png";
-import scooter from "../assets/images/feedersignup.png";
 import profile from "../assets/images/fileicon.png";
 import location from "../assets/images/locationmarkgreen.png";
 import delivery from "../assets/images/deleiveryiconorange.png";
-import phone from "../assets/images/phoneOrange.png";
+import listviewIcon from "../assets/images/listviewIcon.png";
 import locationarrow from "../assets/images/Locationarrow.png";
 const FeederDeliveryRouteView = () => {
   return (
@@ -15,9 +14,21 @@ const FeederDeliveryRouteView = () => {
           <img src={mapBg} alt="" className="img-fluid" height="100%" />
         </div>
       </div>
-      <div className={`${style.mapMenu} shadow`}>menu icon</div>
-      <div className={`${style.gpsMenu} shadow`}>
-        <img src={locationarrow} alt="" />
+      <div className={`${style.mapMenu}`}>
+        {" "}
+        <img src={listviewIcon} alt="" />
+      </div>
+      <div className={`${style.gpsMenu} pt-2 pl-2`}>
+        <img src={locationarrow} alt="" className="img-fluid" />{" "}
+      </div>
+      <div className={`${style.search}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto mt-5">
+              <input type="text" className="form-control" placeholder='Zip code'/>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={`${style.menu} shadow mx-auto px-4`}>
         {" "}
